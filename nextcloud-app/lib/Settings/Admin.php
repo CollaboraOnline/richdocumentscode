737d89f4c7cdcea0982bcca248732b6cad01b20f
Muhammet Kara <muhammet.kara@collabora.com>
2020-06-02 13:06:30 +0300

    Add the Admin Settings page
    Signed-off-by: Muhammet Kara <muhammet.kara@collabora.com>

---
+++ nextcloud-app/lib/Settings/Admin.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ * Copyright (C) 2020 Collabora Productivity, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace OCA\RichDocumentsCODE\Settings;
+
+use OCP\AppFramework\Http\TemplateResponse;
+use OCP\IConfig;
+use OCP\Settings\ISettings;
+
+class Admin implements ISettings
+{
+
+    /** @var IConfig */
+    private $config;
+
+    /**
+     * Admin constructor.
+     *
+     * @param IConfig $config
+     */
+    public function __construct(IConfig $config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * @return TemplateResponse
+     */
+    public function getForm()
+    {
+        $isEnabled = 'no';
+        $absoluteUrl = '';
+
+        $urlGenerator = \OC::$server->getURLGenerator();
+        $absoluteUrl = $urlGenerator->getAbsoluteURL('/settings/apps/app-bundles/richdocuments');
+
+        if (\OC::$server->getAppManager()->isEnabledForUser('richdocuments')) {
+            $isEnabled = 'yes';
+        }
+
+        $parameters = [
+            'richdocumentsEnabled'    => $isEnabled,
+            'richdocumentsURL'    => $absoluteUrl
+        ];
+
+        return new TemplateResponse('richdocumentscode', 'admin', $parameters);
+    }
+
+    /**
+     * @return string the section ID, e.g. 'sharing'
+     */
+    public function getSection()
+    {
+        return 'richdocumentscode';
+    }
+
+    /**
+     * @return int whether the form should be rather on the top or bottom of
+     * the admin section. The forms are arranged in ascending order of the
+     * priority values. It is required to return a value between 0 and 100.
+     */
+    public function getPriority()
+    {
+        return 50;
+    }
+}
---
+++ nextcloud-app/lib/Settings/Section.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ * Copyright (C) 2020 Collabora Productivity, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace OCA\RichDocumentsCODE\Settings;
+
+use OCP\IURLGenerator;
+use OCP\Settings\IIconSection;
+
+class Section implements IIconSection
+{
+    /** @var IURLGenerator */
+    private $url;
+
+    public function __construct(IURLGenerator $url)
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * returns the ID of the section. It is supposed to be a lower case string,
+     * e.g. 'ldap'
+     *
+     * @returns string
+     */
+    public function getID()
+    {
+        return 'richdocumentscode';
+    }
+
+    /**
+     * returns the translated name as it should be displayed, e.g. 'LDAP / AD
+     * integration'. Use the L10N service to translate it.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return "Built-in CODE Server";
+    }
+
+    /**
+     * @return int whether the form should be rather on the top or bottom of
+     * the settings navigation. The sections are arranged in ascending order of
+     * the priority values. It is required to return a value between 0 and 99.
+     *
+     * E.g.: 70
+     */
+    public function getPriority()
+    {
+        return 50;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getIcon()
+    {
+        return $this->url->imagePath('richdocumentscode', 'app-dark.svg');
+    }
+}
