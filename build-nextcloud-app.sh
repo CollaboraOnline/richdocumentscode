ac36c160011e5d8610ad8e8af97671640dd89eab
Muhammet Kara <muhammet.kara@collabora.com>
2020-09-07 12:39:42 +0300

    arm64: l10n & ui bits

--- build-nextcloud-app.sh
+++ build-nextcloud-app.sh
@@ -59,11 +59,19 @@ if [[ "$APPIMAGE_URL" =~ "arm64" ]]; then
     app_name=richdocumentscode_arm64
 
     sed -i.bak "s/x86_64/aarch64/g" ${app_name}/proxy.php
-    sed -i.bak -e 's/<id>richdocumentscode/<id>richdocumentscode_arm64/g' -e 's/Built-in CODE Server/Built-in CODE Server for ARM64/g' ${app_name}/appinfo/info.xml
+    sed -i.bak -e 's/<id>richdocumentscode/<id>richdocumentscode_arm64/g' -e 's/Built-in CODE Server/Built-in CODE Server (ARM64)/g' ${app_name}/appinfo/info.xml
+    sed -i.bak "s/richdocumentscode/richdocumentscode_arm64/g" ${app_name}/lib/Settings/Admin.php
+    sed -i.bak "s/richdocumentscode/richdocumentscode_arm64/g" ${app_name}/lib/Settings/Section.php
+
+    for f in ${app_name}/l10n/*.js; do
+    sed -i.bak "s/richdocumentscode/richdocumentscode_arm64/g" "$f"
+    done
 
     # clean-up
     rm ${app_name}/proxy.php.bak
     rm ${app_name}/appinfo/info.xml.bak
+    rm ${app_name}/l10n/*.bak
+    rm ${app_name}/lib/Settings/*.bak
 fi
 
 echo "Signing…"
