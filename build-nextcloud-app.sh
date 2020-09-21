91e2ec32554d80c580cb340511288426a64fc009
Muhammet Kara <muhammet.kara@collabora.com>
2020-09-21 15:11:13 +0300

    Proper architecture dependency for arm64

--- build-nextcloud-app.sh
+++ build-nextcloud-app.sh
@@ -58,6 +58,7 @@ if [[ "$APPIMAGE_URL" =~ "arm64" ]]; then
     app_name=richdocumentscode_arm64
 
     sed -i.bak "s/x86_64/aarch64/g" ${app_name}/proxy.php
+    sed -i.bak "s/x86_64/aarch64/g" ${app_name}/appinfo/info.xml
     sed -i.bak -e 's/<id>richdocumentscode/<id>richdocumentscode_arm64/g' -e 's/Built-in CODE Server/Built-in CODE Server (ARM64)/g' ${app_name}/appinfo/info.xml
     sed -i.bak "s/richdocumentscode'/richdocumentscode_arm64'/g" ${app_name}/lib/Settings/Admin.php
     sed -i.bak "s/richdocumentscode/richdocumentscode_arm64/g" ${app_name}/lib/Settings/Section.php
